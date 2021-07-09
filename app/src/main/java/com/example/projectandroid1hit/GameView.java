aeac361b78e759dbf662a62b4062a1e7789cfe7b
nofarBarav <br.love.nb@gmail.com>
2021-07-09 19:18:13 +0300

    רואה?

--- app/src/main/java/com/example/projectandroid1hit/GameView.java
+++ app/src/main/java/com/example/projectandroid1hit/GameView.java
@@ -1,7 +1,9 @@
 package com.example.projectandroid1hit;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.view.SurfaceView;
+
 
 public class GameView extends SurfaceView implements Runnable{
     public GameView(Context context) {
@@ -10,6 +12,21 @@ public class GameView extends SurfaceView implements Runnable{
 
     private Thread thread;
     private boolean isPlaying;
+    private int screenX,screenY;
+    private Background background1,background2; //2 background to mack the background move
+
+
+    public GameView(Context context,int screenX,int screenY){
+        super(context);
+
+        this.screenX=screenX;
+        this.screenY=screenY;
+
+        background1= new Background(screenX, screenY, getResources());
+        background2= new Background(screenX, screenY, getResources());
+
+        background2.x = screenX;
+    }
 
     @Override
     public void run() {
@@ -22,16 +39,29 @@ public class GameView extends SurfaceView implements Runnable{
     }
 
     private void update(){
+        background1.x -=10;     //movement to left by 10pix
+        background2.x -=10;
 
-
+        if (background1.x+background1.background.getWidth() < 0) {//if is off the screen
+            background1.x=screenX;
+        }
+        if (background2.x+background2.background.getWidth() < 0) {//if is off the screen
+            background2.x=screenX;
+        }
     }
     private void drew(){
+        if (getHolder().getSurface().isValid()){
+            Canvas canvas= getHolder().lockCanvas();  //Returns the canvas displayed on the screen
 
+        }
 
     }
     private void sleep(){
-
-
+        try {
+            Thread.sleep(17);
+        }        catch (InterruptedException e){
+            e.printStackTrace();
+        }
     }
 
 
